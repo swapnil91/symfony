2d4d93de4a48c0b89aff6a1f8a5568cd07283992
Swapnil Alcon <swapnil.srivastava@alcon.com>
2023-11-13 17:42:28 +0530

    Update render method which uses response object inbulit and passed a variable to twig

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,24 +2,40 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class VinylController
-{
-    #[Route('/')]
-    public function homepage(): Response
-    {
-        // die('Testing symfony controller & router.');
-        // Symfony HttpFoundation library has lots of classes related to Request & Response.
-        // Response can be anything HTML/JSON or String.
-        return new Response('Test Title: Music Director Pritam');
+/**
+ * VinylController Class.
+ *
+ * AbsctractController gives us a shortcut methods like render
+ * thats why extending the same.
+ */
+class VinylController extends AbstractController {
+
+  /**
+   * Homepage url /.
+  */
+  #[Route('/')]
+    public function homepage(): Response {
+    // die('Testing symfony controller & router.');
+    // Symfony HttpFoundation library has lots of classes related to Request
+    // & Response, Response can be anything HTML/JSON or String.
+    // Symfony method should always return a Response.
+    // render method internally do the same.
+    // return new Response('Test Title: Music Director Pritam');.
+    return $this->render('vinyl/homepage.html.twig', [
+      'title' => 'Jack & Jones',
+    ]);
     }
 
+    /**
+     * Dynamic URL /browser/{anything}.
+    */
     #[Route('/browse/{slug}')]
-    public function browse($slug = null): Response
-    {
-        return new Response('Genre: '.$slug);
+    public function browse(string $slug = NULL): Response {
+      return new Response('Genre: ' . $slug);
     }
 
 }
